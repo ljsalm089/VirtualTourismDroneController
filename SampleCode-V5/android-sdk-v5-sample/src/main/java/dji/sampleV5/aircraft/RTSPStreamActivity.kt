329dc1cf96667290790dfae33966b44c67986fe1
alban <ribetalban@gmail.com>
2025-01-31 12:32:16 +1300

    stable version of the app (forgot to commit earlier so I have added an extra activity to teh manifest as well as an empty class for that activity - CameraStreamActivity) but if needed if progress doesnt work and checking out to here just delete from those two files)

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/RTSPStreamActivity.kt
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/RTSPStreamActivity.kt
@@ -7,26 +7,15 @@ import androidx.core.view.WindowInsetsCompat
 import dji.sampleV5.aircraft.pages.CameraStreamDetailFragment
 import dji.sampleV5.aircraft.pages.CameraStreamListFragment
 
-class LiveStreamActivity : AppCompatActivity() {
+class RTSPStreamActivity :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
-        setContentView(R.layout.activity_stream_live)
+        setContentView(R.layout.activity_stream_rtsp)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val cameraStreamDetailFragment = CameraStreamDetailFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, cameraStreamDetailFragment)
-            .commit()
-
-        val cameraStreamListFragment = CameraStreamListFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment2_container, cameraStreamListFragment)
-            .commit()
-
     }
 }
