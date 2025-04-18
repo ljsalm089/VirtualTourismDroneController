e73b778b57a4b121218b156901add89c4bb9d8f1
Json Lee <ljsalm089@gmail.com>
2025-04-19 09:38:13 +1200

    feat: shift some logic from CameraStreamActivity to other classes

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/models/CameraStreamVM.kt
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/models/CameraStreamVM.kt
@@ -1,7 +1,86 @@
 package dji.sampleV5.aircraft.models
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dji.sampleV5.aircraft.DJIVideoCapturer
+import dji.sampleV5.aircraft.webrtc.ConnectionInfo
+import dji.sampleV5.aircraft.webrtc.DataFromChannel
+import dji.sampleV5.aircraft.webrtc.EVENT_CREATE_CONNECTION_ERROR
+import dji.sampleV5.aircraft.webrtc.EVENT_CREATE_CONNECTION_SUCCESS
+import dji.sampleV5.aircraft.webrtc.EVENT_EXCHANGE_OFFER_ERROR
+import dji.sampleV5.aircraft.webrtc.EVENT_RECEIVED_DATA
+import dji.sampleV5.aircraft.webrtc.VIDEO_PUBLISHER
+import dji.sampleV5.aircraft.webrtc.WebRtcEvent
+import dji.sampleV5.aircraft.webrtc.WebRtcManager
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.functions.Consumer
+import org.webrtc.AudioSource
+import org.webrtc.VideoCapturer
+import org.webrtc.VideoSource
 
-class CameraStreamVM : ViewModel() {
+private const val useDroneCamera = false
 
+class CameraStreamVM(application: Application) : ViewModel(), Consumer<WebRtcEvent> {
+
+    private var webRtcManager: WebRtcManager = WebRtcManager(scope = viewModelScope, application)
+    private val eventDisposable: Disposable =
+        webRtcManager.webRtcEventObservable.observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this)
+
+    private var videoCapturer: VideoCapturer? = null
+
+    private var videoSource: VideoSource? = null
+    private var audioSource: AudioSource? = null
+
+    init {
+
+    }
+
+    fun startPublish() {
+        webRtcManager.start()
+    }
+
+    fun stopPublish() {
+        webRtcManager.stop()
+    }
+
+    fun sendData() {
+
+    }
+
+    override fun accept(t: WebRtcEvent) {
+        if (EVENT_CREATE_CONNECTION_SUCCESS == t.event) {
+            val connectionInfo = t.data as? ConnectionInfo
+            if (VIDEO_PUBLISHER == connectionInfo?.identity) {
+                attachVideoAndAudioToConnection(connectionInfo)
+            }
+        } else if (EVENT_CREATE_CONNECTION_ERROR == t.event) {
+            // create connection error, the data is null
+        } else if (EVENT_EXCHANGE_OFFER_ERROR == t.event) {
+            if (t.data is Exception) {
+
+            } else {
+                // string
+            }
+        } else if (EVENT_RECEIVED_DATA == t.event) {
+            val data = t.data as? DataFromChannel
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        eventDisposable.dispose()
+
+        webRtcManager.stop()
+    }
+
+    private fun attachVideoAndAudioToConnection(connectionInfo: ConnectionInfo) {
+        if (useDroneCamera) {
+            videoCapturer = DJIVideoCapturer()
+            videoSource =
+                connectionInfo.connectionFactory.createVideoSource(videoCapturer!!.isScreencast)
+        }
+    }
 }
--- SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/webrtc/WebRtcManager.kt
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/webrtc/WebRtcManager.kt
@@ -1,10 +1,29 @@
 package dji.sampleV5.aircraft.webrtc
 
+import android.app.Application
+import android.util.Log
+import io.reactivex.rxjava3.subjects.PublishSubject
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import org.webrtc.DataChannel
+import org.webrtc.DefaultVideoDecoderFactory
+import org.webrtc.DefaultVideoEncoderFactory
+import org.webrtc.EglBase
+import org.webrtc.IceCandidate
+import org.webrtc.MediaConstraints
+import org.webrtc.MediaStream
+import org.webrtc.PeerConnection
+import org.webrtc.PeerConnection.IceServer
+import org.webrtc.PeerConnection.Observer
+import org.webrtc.PeerConnection.RTCConfiguration
+import org.webrtc.PeerConnectionFactory
+import org.webrtc.SdpObserver
+import org.webrtc.SessionDescription
+import java.nio.ByteBuffer
 
 
 const val TYPE_DATA = "data"
@@ -13,18 +32,103 @@ const val TYPE_AUDIO = "audio"
 
 const val TYPE_VIDEO = "video"
 
+const val VIDEO_PUBLISHER = "videoPublisher"
+
+const val DATA_RECEIVER = "dataReceiver"
+
+const val STUN_SERVER = "stun:stun.l.google.com:19302"
+
+private const val TAG = "WebRtcManager"
+
+const val EVENT_CREATE_CONNECTION_ERROR = "create_connection_error"
+const val EVENT_CREATE_CONNECTION_SUCCESS = "create_connection_success"
+const val EVENT_EXCHANGE_OFFER_ERROR = "exchange_offer_error"
+
+const val EVENT_RECEIVED_DATA = "received_data"
 
 
-class WebRtcManager(private val scope: CoroutineScope) {
+data class WebRtcEvent(val event: String, val data: Any?)
+
+data class ConnectionInfo(
+    val identity: String,
+    val connection: PeerConnection,
+    val eglBase: EglBase,
+    val connectionFactory: PeerConnectionFactory
+)
+
+data class DataFromChannel(val identity: String, val data: String, val channel: String)
+
+interface IWebRtcEventEmitter {
+
+    fun emit(event: WebRtcEvent)
+
+}
+
+interface IConnectionFactoryProvider {
+
+    fun get(): PeerConnectionFactory
+
+}
+
+class WebRtcManager(private val scope: CoroutineScope, private val application: Application) :
+    IWebRtcEventEmitter, IConnectionFactoryProvider {
+
+    val webRtcEventObservable = PublishSubject.create<WebRtcEvent>()
 
     private lateinit var periodicJob: Job
 
-    private val connections = HashSet<WebRtcConnection>()
+    private val connections = HashMap<String, WebRtcConnection>()
+
+    private val eglBase = EglBase.create()
+
+    private val peerConnectionFactory: PeerConnectionFactory
+
+    init {
+        val option = PeerConnectionFactory.InitializationOptions.builder(application)
+            .setEnableInternalTracer(true)
+            .createInitializationOptions()
+        PeerConnectionFactory.initialize(option)
+
+        peerConnectionFactory = PeerConnectionFactory.builder()
+            .setVideoDecoderFactory(DefaultVideoDecoderFactory(eglBase.eglBaseContext))
+            .setVideoEncoderFactory(DefaultVideoEncoderFactory(eglBase.eglBaseContext, true, true))
+            .createPeerConnectionFactory()
+    }
 
     fun start() {
+        // create a connection for publishing video
+        var supportTypes = hashSetOf(
+            TYPE_VIDEO,
+            TYPE_AUDIO,
+            TYPE_DATA
+        )
+        val whipOfferExchange = WhipOfferExchanger(VIDEO_PUBLISHER)
+        var conn = WebRtcConnection(
+            VIDEO_PUBLISHER, supportTypes, whipOfferExchange,
+            this, this, eglBase, scope
+        )
+        connections[VIDEO_PUBLISHER] = conn
 
+        // create a connection for receiving data
+        supportTypes = hashSetOf(
+            TYPE_DATA
+        )
+        val whepOfferExchange = WhepOfferExchanger(DATA_RECEIVER)
+        conn = WebRtcConnection(
+            DATA_RECEIVER, supportTypes, whepOfferExchange,
+            this, this, eglBase, scope
+        )
+        connections[DATA_RECEIVER] = conn
+
+        for (keyAndValue in connections.entries) {
+            keyAndValue.value.connect()
+        }
 
         executePeriodicTask()
+    }
+
+    fun sendData(data: String) {
+        connections[VIDEO_PUBLISHER]?.sendData(data)
     }
 
     fun stop() {
@@ -32,18 +136,26 @@ class WebRtcManager(private val scope: CoroutineScope) {
             this.periodicJob.cancel()
         }
 
-        for (connection: WebRtcConnection in connections) {
-            connection.disconnect()
+        for (keyAndValue in connections.entries) {
+            keyAndValue.value.disconnect()
         }
         connections.clear()
+    }
+
+    override fun emit(event: WebRtcEvent) {
+        webRtcEventObservable.onNext(event)
+    }
+
+    override fun get(): PeerConnectionFactory {
+        return peerConnectionFactory
     }
 
     private fun executePeriodicTask() {
         periodicJob = scope.launch {
             while (isActive) {
                 // keep alive
-                for (connection: WebRtcConnection in connections) {
-                    connection.keepAlive()
+                for (keyAndValue in connections) {
+                    keyAndValue.value.keepAlive()
                 }
 
                 delay(1000)
@@ -52,18 +164,239 @@ class WebRtcManager(private val scope: CoroutineScope) {
     }
 }
 
-class WebRtcConnection(val identity: String, val supportTypes: Set<String>, val offerExchange: IOfferExchange) {
+class WebRtcConnection(
+    private val identity: String,
+    private val supportTypes: Set<String>,
+    private val offerExchange: IOfferExchange,
+    private val eventEmitter: IWebRtcEventEmitter,
+    private val connectionFactoryProvider: IConnectionFactoryProvider,
+    private val eglBase: EglBase,
+    private val scope: CoroutineScope,
+) {
 
+    lateinit var connection: PeerConnection
+    private val receivedChannels: HashMap<String, DataChannel> = HashMap()
+    private val dataObservers: HashMap<String, DataChannel.Observer> = HashMap()
+    private var transmitDataChannel: DataChannel? = null
 
     fun connect() {
+        val iceServers = listOf(
+            IceServer(STUN_SERVER)
+        )
+        val rtcConfig = RTCConfiguration(iceServers)
+        rtcConfig.sdpSemantics = PeerConnection.SdpSemantics.UNIFIED_PLAN
 
+        val conn = connectionFactoryProvider.get().createPeerConnection(rtcConfig, object : Observer {
+            override fun onSignalingChange(p0: PeerConnection.SignalingState?) {
+                Log.d(TAG, "onSignalingChange: $p0")
+            }
+
+            override fun onIceConnectionChange(p0: PeerConnection.IceConnectionState?) {
+                Log.d(TAG, "ICE Connection State: $p0")
+            }
+
+            override fun onIceConnectionReceivingChange(p0: Boolean) {
+                Log.d(TAG, "onIceConnectionReceivingChange: $p0")
+            }
+
+            override fun onIceGatheringChange(p0: PeerConnection.IceGatheringState?) {
+                Log.d(TAG, "onIceGatheringChange: $p0")
+            }
+
+            override fun onIceCandidate(p0: IceCandidate?) {
+                Log.d(TAG, "ICE Candidate: ${p0?.sdp}")
+            }
+
+            override fun onIceCandidatesRemoved(p0: Array<out IceCandidate>?) {
+                Log.d(TAG, "onIceCandidatesRemoved")
+            }
+
+            override fun onAddStream(p0: MediaStream?) {
+            }
+
+            override fun onRemoveStream(p0: MediaStream?) {
+            }
+
+            override fun onDataChannel(p0: DataChannel?) {
+                p0?.let {
+                    onReceivedDataChannel(it)
+                }
+            }
+
+            override fun onRenegotiationNeeded() {
+                Log.d(TAG, "onRenogotiationNeeded")
+            }
+        })
+        if (null == conn) {
+            eventEmitter.emit(WebRtcEvent(EVENT_CREATE_CONNECTION_ERROR, null))
+            return
+        }
+        connection = conn
+        eventEmitter.emit(
+            WebRtcEvent(
+                EVENT_CREATE_CONNECTION_SUCCESS,
+                ConnectionInfo(identity, connection, eglBase, connectionFactoryProvider.get())
+            )
+        )
+        if (supportTypes.contains(TYPE_DATA)) {
+            // create the test channel to make sure the offer contains the information to support data channels
+            val init = DataChannel.Init()
+            init.ordered = true
+            init.protocol = "json"
+            init.id = 10000
+            connection.createDataChannel("test", init)
+        }
+
+        connection.createOffer(object : SdpObserver {
+            override fun onCreateSuccess(p0: SessionDescription?) {
+                p0?.let {
+                    connection.setLocalDescription(this, p0)
+                    return
+                }
+
+                eventEmitter.emit(
+                    WebRtcEvent(
+                        EVENT_EXCHANGE_OFFER_ERROR,
+                        "Unable to create valid local offer!"
+                    )
+                )
+            }
+
+            override fun onSetSuccess() {
+                exchangeOffer(connection.localDescription)
+            }
+
+            override fun onCreateFailure(p0: String?) {
+                eventEmitter.emit(
+                    WebRtcEvent(
+                        EVENT_EXCHANGE_OFFER_ERROR,
+                        "Unable to create local offer!"
+                    )
+                )
+            }
+
+            override fun onSetFailure(p0: String?) {
+                eventEmitter.emit(
+                    WebRtcEvent(
+                        EVENT_EXCHANGE_OFFER_ERROR,
+                        "Unable to set local offer!"
+                    )
+                )
+            }
+        }, MediaConstraints())
     }
 
     fun keepAlive() {
+        scope.launch(Dispatchers.IO) {
+            for (kvp in receivedChannels.entries) {
+                kvp.value.send(DataChannel.Buffer(ByteBuffer.wrap("Ping".toByteArray()), false))
+            }
+        }
+    }
 
+    fun sendData(data: String) {
+        if (!this::connection.isInitialized) return
+
+        if (null == transmitDataChannel) {
+            val init = DataChannel.Init()
+            init.ordered = true
+            init.id = 1
+            init.protocol = "json"
+            transmitDataChannel = connection.createDataChannel("DronePosFeedBack", init)
+            transmitDataChannel?.registerObserver(object : DataChannel.Observer {
+                override fun onBufferedAmountChange(p0: Long) {
+                }
+
+                override fun onStateChange() {
+                    if (transmitDataChannel?.state() == DataChannel.State.CLOSED) {
+                        transmitDataChannel = null
+                    }
+                }
+
+                override fun onMessage(p0: DataChannel.Buffer?) {
+                    // in theory, won't receive any data from this channel, just ignore it
+                }
+            })
+        }
+        transmitDataChannel?.send(DataChannel.Buffer(ByteBuffer.wrap(data.toByteArray()), false))
     }
 
     fun disconnect() {
+        if (!this::connection.isInitialized) return
 
+        for (kav in receivedChannels.entries) {
+            kav.value.dispose()
+        }
+        this.transmitDataChannel?.let {
+            it.dispose()
+            this.transmitDataChannel = null
+        }
+        this.connection.dispose()
     }
+
+    private fun onReceivedDataChannel(channel: DataChannel) {
+        if (receivedChannels.contains(channel.label())) return
+
+        receivedChannels[channel.label()] = channel
+
+        val dataObserver = object : DataChannel.Observer {
+            override fun onBufferedAmountChange(p0: Long) {
+                Log.d(TAG, "onBufferedAmountChange: $p0")
+            }
+
+            override fun onStateChange() {
+                Log.d(TAG, "onStateChange: ${channel.state()}")
+            }
+
+            override fun onMessage(p0: DataChannel.Buffer?) {
+                p0?.let {
+                    val data = it.data.array().decodeToString()
+
+                    eventEmitter.emit(
+                        WebRtcEvent(
+                            EVENT_RECEIVED_DATA, DataFromChannel(identity, data, channel.label())
+                        )
+                    )
+                }
+            }
+
+        }
+        channel.registerObserver(dataObserver)
+        dataObservers[channel.label()] = dataObserver
+    }
+
+    private fun exchangeOffer(offer: SessionDescription) {
+        scope.launch(Dispatchers.IO) {
+            try {
+                val remoteOffer = offerExchange.exchangeOffer(offer.description)
+                scope.launch(Dispatchers.Main) { setRemoteOffer(remoteOffer) }
+            } catch (e: Exception) {
+                eventEmitter.emit(WebRtcEvent(EVENT_EXCHANGE_OFFER_ERROR, "Network error: $e"))
+            }
+        }
+    }
+
+    private fun setRemoteOffer(offer: String) {
+        connection.setRemoteDescription(object : SdpObserver {
+            override fun onCreateSuccess(p0: SessionDescription?) {
+            }
+
+            override fun onSetSuccess() {
+            }
+
+            override fun onCreateFailure(p0: String?) {
+            }
+
+            override fun onSetFailure(p0: String?) {
+                eventEmitter.emit(
+                    WebRtcEvent(
+                        EVENT_EXCHANGE_OFFER_ERROR,
+                        "Unable to set remote offer"
+                    )
+                )
+            }
+
+        }, SessionDescription(SessionDescription.Type.ANSWER, offer))
+    }
+
 }
